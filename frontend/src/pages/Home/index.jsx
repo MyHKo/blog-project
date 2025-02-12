5b2f44a997a8be8363d95ca2b1392c2159ecfc79
Mykola Horbenko <mykola.horbenko22@gmail.com>
2025-02-12 14:05:06 +0100

    Frontend: add homepage

--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -1,8 +1,11 @@
 import styles from './home.module.scss';
 
 function Home() {
-    return (<div className={styles.home_container}>
-    </div>)
+    return (
+        <div className={styles.home_container}>
+            <h1 className={styles.title}>Latest Blog Posts</h1>
+        </div>
+    )
 }
 
 export default Home;
