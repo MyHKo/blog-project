5da760659a1c8a7ccc02544c7453f89848c98476
Mykola Horbenko <mykola.horbenko22@gmail.com>
2025-02-14 16:18:33 +0100

    Frontend[FEAT-1]: add background image

--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -4,8 +4,18 @@ import styles from './home.module.scss';
 function Home() {
     return (
         <div className={styles.container}>
+            <div className={styles.image_container}>
+            </div>
             <h1 className={styles.title}>Latest Blog Posts</h1>
-            <Post />
+            {/*<Post />*/}
+            {/*        <Post />*/}
+            {/*        <Post />*/}
+            {/*        <Post />*/}
+            {/*        <Post />*/}
+            {/*        <Post />*/}
+            {/*        <Post />*/}
+            {/*        <Post />*/}
+            {/*        <Post />*/}
         </div>
     )
 }
