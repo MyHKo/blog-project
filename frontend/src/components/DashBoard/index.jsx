7abe8a1bee4f2f0bbad5844117242e0625047c95
Mykola Horbenko <mykola.horbenko22@gmail.com>
2025-02-16 13:30:58 +0100

    Frontend: add functionality to the side menu component

--- frontend/src/components/DashBoard/index.jsx
+++ frontend/src/components/DashBoard/index.jsx
@@ -1,16 +1,27 @@
 import SideMenu from "@components/DashBoard/components/SideMenu/index.jsx";
 import SideMenuButton from "@components/DashBoard/components/SideMenuButton";
 import SideMenuMobile from "@components/DashBoard/components/SideMenuMobile";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 
 function DashBoard() {
     const [isSideMenuVisible, setIsSideMenuVisible] = useState(false);
+    const [isScreenSmall, setIsScreenSmall] = useState(window.matchMedia("(min-width: 980px)").matches);
+
+    useEffect(() => {
+        const mediaQuery = window.matchMedia("(min-width: 980px)");
+
+    const handleResize = () => setIsScreenSmall(mediaQuery.matches);
+    mediaQuery.addEventListener("change", handleResize);
+
+    return () => mediaQuery.removeEventListener("change", handleResize);
+}, []);
 
     return (
         <div>
             <SideMenuButton isSideMenuVisible={isSideMenuVisible} setIsSideMenuVisible={setIsSideMenuVisible} />
-            <SideMenuMobile isVisible={isSideMenuVisible} setIsVisible={setIsSideMenuVisible}/>
-            {/*<SideMenu />*/}
+            {isScreenSmall
+                ? <SideMenu />
+                : <SideMenuMobile isVisible={isSideMenuVisible} setIsVisible={setIsSideMenuVisible}/>}
         </div>
     )
 }
