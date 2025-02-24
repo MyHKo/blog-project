ff1a8852968db3b1913589342642e2684a97dc51
Mykola Horbenko <mykola.horbenko22@gmail.com>
2025-02-24 16:24:41 +0100

    Frontend: add placeholder for About page

---
+++ frontend/src/pages/About/index.jsx
@@ -0,0 +1,11 @@
+import styles from './about.module.scss'
+
+function About() {
+    return (
+        <div style={styles.container}>
+
+        </div>
+    )
+}
+
+export default About
--- frontend/src/routing/AppRoutes/index.jsx
+++ frontend/src/routing/AppRoutes/index.jsx
@@ -1,11 +1,13 @@
 import {Route, Routes} from 'react-router-dom'
 import Home from '@pages/Home'
+import About from '@pages/About/';
 import routes from '@routing/path.js'
 
 function AppRoutes() {
     return (
         <Routes>
             <Route path={routes.home} element={<Home />} />
+            <Route path={routes.about} element={<About />} />
         </Routes>
     )
 }
