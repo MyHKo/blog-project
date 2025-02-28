66a8b1b00c7cbec2c6c7e9738bc2ade2bdf73649
Mykola Horbenko <mykola.horbenko22@gmail.com>
2025-02-28 16:25:24 +0100

    Frontend: add a todo task to navbar

--- frontend/src/components/DashBoard/components/SideMenu/components/NavBar/index.jsx
+++ frontend/src/components/DashBoard/components/SideMenu/components/NavBar/index.jsx
@@ -5,6 +5,10 @@ import styles from './navbar.module.scss'
 function NavBar() {
     const navigate = useNavigate()
 
+    const navigateOutside = () => {
+        //TODO: add a link to kickstarter
+    }
+
     return (
         <nav>
             <ul className={styles.list}>
@@ -15,9 +19,11 @@ function NavBar() {
                     navigate('/about')
                 }}>About us</span></span>
                 <span className={styles.list_item}><span className={styles.item_text} onClick={() => {
+                    navigateOutside()
+                }}>Kickstarter</span></span>
+                <span className={styles.list_item}><span className={styles.item_text} onClick={() => {
                     navigate('/editor')
                 }}>Add Post</span></span>
-                <span className={styles.list_item}><span className={styles.item_text}>Kickstarter</span></span>
             </ul>
         </nav>
     )
